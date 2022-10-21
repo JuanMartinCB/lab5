b5a2facb9c5704c791ab28afa059b729f146d4c2
juan <juanmac1207@alu.ing.unlp.edu.ar>
2022-10-21 14:43:43 -0300

    rutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :monsters, only: [:index]
-
-  root "monsters#index"
+  resources :monsters
+  resources :tweets, exlude: [:edit, :update, :destroy]
+  root "tweets#index" # / is the same as /tweets
+  resolve("new_tweet_path") {route_for(:new)}
+  resolve("new_monster_path ") {route_for(:new)}
+  #post '/tweets/new', to: 'tweets#create'
 end
