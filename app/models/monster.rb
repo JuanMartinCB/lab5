f036d9b98400e965983cbbc8f44472d600a1353d
juan <juanmac1207@alu.ing.unlp.edu.ar>
2022-10-21 15:22:33 -0300

    phone validator

--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,4 +1,13 @@
 class Monster < ApplicationRecord
+  validate :phone_format
   has_many :tweets
   validates :name, :birthdate, :phone , presence: true, uniqueness: true
+
+  def phone_format
+    if PhoneValidator.new(phone).valid?
+      return
+    else
+      errors.add(:phone, :invalid)
+    end
+  end
 end
---
+++ app/models/phone_validator.rb
@@ -0,0 +1,21 @@
+class PhoneValidator
+    include HTTParty
+    base_uri "https://phonevalidation.abstractapi.com/v1/"
+
+    def initialize(phone)
+        @phone  = phone
+    end
+
+    def valid?
+        response = self.class.get("/", {
+            query: {
+                api_key: Rails.application.credentials.abstractapi_api_key,
+                phone: @phone
+            }
+        })
+        #response is from argentine and valid format
+        response["country_code"] == "AR" && response["valid_format"]
+     
+    end
+
+end
