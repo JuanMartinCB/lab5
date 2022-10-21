02d7901f8209273b6b94934414b7b40c331ec24b
juan <juanmac1207@alu.ing.unlp.edu.ar>
2022-10-21 14:41:12 -0300

    cambios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_04_000934) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_14_112018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_04_000934) do
     t.datetime "updated_at", null: false
     t.string "phone"
     t.date "birthdate"
+  end
+
+  create_table "pepes", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tweets", force: :cascade do |t|
